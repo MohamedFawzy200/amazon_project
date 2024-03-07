ce3ac1d84a137186614a1c5f636e64e43f7a1d95
AhmedAshour19 <ahmedashour2003123@gmail.com>
2024-03-08 01:13:10 +0200

    first project

---
+++ firstproject/amazon/customers/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from customers.views import index,index1
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index/',index),
+    path('index1/',index1),
+]
